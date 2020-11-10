5226d690fc2fefaa1664058a51da114c1c3578c3
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-10 15:51:30 +0800

    Update PS2_i.R
    good work and good to propose some good questions, some recommended materials for you to read:
    http://blog.fens.me/r-tibble/
    
    https://blog.csdn.net/wmm131333/article/details/100691190?utm_medium=distribute.pc_relevant.none-task-blog-title-2&spm=1001.2101.3001.4242

--- PS2_i.R
+++ PS2_i.R
@@ -7,14 +7,27 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 signif <- read.csv("signif.txt", head = TRUE, sep = '\t')
+# @MingYANG recommended：
+# "read.delim" is recommended here, but read.table/read.csv/read.delim don`t have significant differences
+# they are different in the default format and the speed of reading
+# see also:  https://blog.csdn.net/wmm131333/article/details/100691190?utm_medium=distribute.pc_relevant.none-task-blog-title-2&spm=1001.2101.3001.4242
+# the end
+
 # sign1 <- read.table("signif.txt",header = T, fill = T)  this can also read file but data structure is strange
 # fill = T ?
 Sig_Eqs <- as_tibble(signif)
 #1.2
 top_ten_death <- Sig_Eqs %>%
   select(COUNTRY, YEAR, DEATHS) %>%
+#@MingYANG recommended:
+# the colum of "YEAR" was not used
+# the end 
     group_by(COUNTRY) %>%
      summarise( death_number = sum(DEATHS)) %>%
+#@MingYANG noticed:
+# the data with the value of "NA" should be noticed and removed
+# replacing line 20 with "summarise( death_number = sum(DEATHS,na.rm=T)) %>%" so as to avoid "NA" value
+# the end
       arrange(desc(death_number))
 top_ten_death[1:10,]
 #1.3
@@ -53,6 +66,13 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 Shenzhen_data <- read.csv("2281305.csv", head = TRUE)
+# @MingYANG recommended:
+# R save data as "data.frame" format for reading data from csv file directly
+# using as_tible to convert the data format into "tbl_df"
+# "tbl_df" is based on "data.frame" so it has downward compatibility
+# but "tbl_df" is much quick than "data.frame"
+# see also: http://blog.fens.me/r-tibble/
+# the end
 Shenzhen_data %>%
   select(DATE,WND) %>%
   mutate(YM = substr(DATE,1,7), WD = as.numeric(substr(WND,9,12))) %>%
